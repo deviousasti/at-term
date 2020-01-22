5d9b3f4118b0e440cc8fac5152197d6c581051bf
asti <rahul@webyfy.com>
2020-01-22 22:39:33 +0530

    Refactor AtCommand

--- src/AtCommand.cs
+++ src/AtCommand.cs
@@ -1,40 +1,40 @@
 ﻿using System;
 using System.Linq;
+using System.Windows;
 
 namespace AtTerm
 {
 
-    public partial class TermViewModel
+    public class AtCommand
     {
-        public class AtCommand
+        public AtCommand(string command, string args, bool isAT = false)
         {
-            public AtCommand(string command, string args, bool isAT = false)
-            {
-                Command = command;
-                Args = args;
-                IsAT = isAT;
-            }
-
-            public string Command { get; }
-            public string Args { get; }
-            public bool IsAT { get; }
-
-            public bool IsValid => !String.IsNullOrWhiteSpace(Command);
-
-
-            public static AtCommand Parse(string raw)
-            {
-                var line = raw.Trim();
-                var isAT = line.StartsWith("AT+");
-                var args = (isAT ? line.Substring(3) : line).Split(new[] { '=' }, 2, StringSplitOptions.RemoveEmptyEntries);
-                var command = args.FirstOrDefault();
-
-
-                return new AtCommand(args.FirstOrDefault(), args.LastOrDefault(), isAT);
-            }
-
-            public override string ToString() => Command;
-
+            Command = command;
+            Args = args;
+            IsAT = isAT;
         }
+
+        public string Command { get; }
+        public string Args { get; }
+        public bool IsAT { get; }
+
+        public bool IsValid => !String.IsNullOrWhiteSpace(Command);
+
+        public static bool IsValidCommand(string command) => !String.IsNullOrWhiteSpace(command);
+
+
+        public static AtCommand Parse(string raw)
+        {
+            var line = raw.Trim();
+            var isAT = line.StartsWith("AT+");
+            var args = (isAT ? line.Substring(3) : line).Split(new[] { '=' }, 2, StringSplitOptions.RemoveEmptyEntries);
+            var command = args.FirstOrDefault();
+
+
+            return new AtCommand(args.FirstOrDefault(), args.LastOrDefault(), isAT);
+        }
+
+        public override string ToString() => Command;
     }
+
 }
