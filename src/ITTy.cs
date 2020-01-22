7d194ebeac04b976169e4626c760b7c2c56d064e
asti <rahul@webyfy.com>
2020-01-22 21:51:07 +0530

    Add ITTy interface

---
+++ src/ITTy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AtTerm
+{
+    public interface ITTy
+    {
+        event Action<string> Received;
+        event Action<string> Connected;
+
+        event Action<string> Disconnected;
+        void Send(string text);
+        bool Connect();
+        bool Disconnect();
+        bool IsConnected { get; }
+    }
+}
