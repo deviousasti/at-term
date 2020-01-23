a0acbacbaff36d077bc4ce75210232d7b4fd1e7a
asti <rahul@webyfy.com>
2020-01-23 13:45:50 +0530

    Add shortcut bindings

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -24,46 +24,17 @@ namespace AtTerm
         {
             InitializeComponent();
             this.DataContext = viewModel;
-
         }
 
-        public TermViewModel ViewModel => this.DataContext as TermViewModel;
-
-        private void OnSubmit(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                if (e.KeyboardDevice.Modifiers == ModifierKeys.Control)
-                    ViewModel.OnContinuation();
-                else
-                    ViewModel.OnSubmit();
-            }
-
-            if (e.Key == Key.Tab)
-            {
-                ViewModel.AutoCompleteText();
-                CommandInput.SetCaretPosition(int.MaxValue);
-                e.Handled = true;
-            }
-
-            if (e.Key == Key.Up)
-            {
-                ViewModel.CycleHistory();
-                CommandInput.SetCaretPosition(int.MaxValue);
-            }
-
-            if (e.Key == Key.Escape)
-            {
-                ViewModel.ClearText();
-            }
-
-        }
 
         protected override void OnClosed(EventArgs e)
         {
             base.OnClosed(e);
             ViewModel.OnExit();
         }
+
+
+        public TermViewModel ViewModel => this.DataContext as TermViewModel;
 
         private void FavouriteItemClicked(object sender, RoutedEventArgs e)
         {
@@ -87,7 +58,76 @@ namespace AtTerm
                     if (listView.Items.Count > 0)
                         listView.ScrollIntoView(listView.Items.GetItemAt(listView.Items.Count - 1));
                 }
+            }
+        }
 
+        private void OnListViewDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ViewModel.Send();
+        }
+
+        private void OnSubmit(object sender, KeyEventArgs e)
+        {
+            var hasCtrl = e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control);
+
+            if (e.Key == Key.Enter)
+            {
+                if (hasCtrl)
+                    ViewModel.OnContinuation();
+                else
+                    ViewModel.OnSubmit();
+            }
+
+            if (hasCtrl && e.Key == Key.R)
+            {
+                ViewModel.SendLast();
+            }
+
+            if (e.Key == Key.Tab && !hasCtrl)
+            {
+                ViewModel.AutoCompleteText();
+                CommandInput.SetCaretPosition(int.MaxValue);
+                e.Handled = true;
+            }
+
+            if (e.Key == Key.Up && !CommandInput.IsDropDownOpen && !hasCtrl)
+            {
+                ViewModel.CycleHistory();                
+                CommandInput.SetCaretPosition(int.MaxValue);
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                ViewModel.ClearText();
+            }
+        }
+
+        private void OnListViewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ViewModel.Send();
+            }
+
+            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control))
+            {
+                if (e.Key == Key.C)
+                {
+                    var listview = e.Source as ListView;
+                    var text = String.Join("\r\n", listview.SelectedItems.Cast<Object>());
+                    Clipboard.SetText(text);
+                }
+
+                if (e.Key == Key.V)
+                {
+                    ViewModel.Send(Clipboard.GetText());
+                }
+
+                //if (e.Key == Key.V)
+                //{
+                //    Clipboard.GetText().Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                //    ViewModel.Send();
+                //}
             }
 
         }
